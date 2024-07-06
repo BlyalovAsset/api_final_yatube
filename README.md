9e33de2d23109975ff30396344b86bd2016f427a
Blyalov Asset <Blyalov012@gmail.com>
2024-07-07 02:02:56 +0500

    have done task

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# api_final
-api final
+# API_FINAL_YATUBE
+
+API_Final - законченная версия API для yatube.
+
+## Стек технологий
+
+- Python 3.7
+- Django Rest Framework
+- SQLite
+
+## Как запустить проект
+
+### 1. Клонировать репозиторий и перейти в него в командной строке:
+
+```bash
+git clone git@github.com:LazarevaKate/api_final_yatube.git
+cd api_final_yatube
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import (Comment,
+                          Group,
+                          Follow,
+                          Post,
+                          User)
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True)
 
     class Meta:
         fields = '__all__'
@@ -21,3 +26,37 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ['author', 'post']
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    validators = [UniqueTogetherValidator(
+        queryset=Follow.objects.all(),
+        fields=['user', 'following'])]
+
+    def validate(self, data):
+        if self.context['request'].user != data.get('following'):
+            return data
+        raise serializers.ValidationError(
+            'Нельзя подписаться на себя'
+        )
+
+    class Meta:
+        fields = ('__all__')
+        model = Follow
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('__all__')
