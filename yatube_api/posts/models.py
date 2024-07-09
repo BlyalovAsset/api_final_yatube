ab6f3fd7dfcf1ec23b3b2329b36b3fbe64fad643
Blyalov Asset <Blyalov012@gmail.com>
2024-07-09 23:31:47 +0500

    newaa

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,43 +1,60 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from posts.constant import LENGTH_TEXT, MAX_LENGTH
+
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(
+        verbose_name='Название сообщества',
+        max_length=MAX_LENGTH,
+        db_index=True)
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
-        return self.title
+        return self.title[:LENGTH_TEXT]
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(verbose_name='Запись')
     pub_date = models.DateTimeField(
-        'Дата публикации', auto_now_add=True
+        'Дата публикации',
+        auto_now_add=True
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts'
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
     )
     group = models.ForeignKey(
-        Group, on_delete=models.SET_NULL,
-        related_name="posts", blank=True, null=True
+        Group,
+        on_delete=models.SET_NULL,
+        related_name="posts",
+        blank=True,
+        null=True
     )
 
     def __str__(self):
-        return self.text
+        return self.text[:LENGTH_TEXT]
 
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments')
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments')
     text = models.TextField()
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True)
 
 
 class Follow(models.Model):
