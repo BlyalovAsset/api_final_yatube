07503990a291f55d82da3b828d506e118647df3f
BlyalovAsset <123326319+BlyalovAsset@users.noreply.github.com>
2024-07-11 12:17:47 +0500

    Update urls.py
    commit

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -20,6 +20,19 @@ v1_patterns = [
     path('', include('djoser.urls.jwt')),
 ]
 
+router_v2 = routers.DefaultRouter()
+router_v2.register('posts', PostViewSet, basename='posts')
+router_v2.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet, basename='comments'
+)
+
+v2_patterns = [
+    path('', include(router_v2.urls)),
+    path('', include('djoser.urls.jwt')),
+]
+
 urlpatterns = [
     path('v1/', include(v1_patterns)),
+    path('v2/', include(v2_patterns)),
 ]
