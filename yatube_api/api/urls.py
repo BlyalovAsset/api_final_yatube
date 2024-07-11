4e81d07eed3ef913f4a3fc787414ecc8ac4cbfbd
BlyalovAsset <123326319+BlyalovAsset@users.noreply.github.com>
2024-07-11 18:16:57 +0500

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,33 +6,20 @@ from api.views import (PostViewSet,
                        GroupViewSet,
                        FollowViewSet)
 
-router = routers.DefaultRouter()
-router.register('posts', PostViewSet, basename='posts')
-router.register(
+router_v1 = routers.DefaultRouter()
+router_v1.register('posts', PostViewSet, basename='posts')
+router_v1.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet, basename='comments'
 )
-router.register('groups', GroupViewSet, basename='groups')
-router.register('follow', FollowViewSet, basename='follow')
+router_v1.register('groups', GroupViewSet, basename='groups')
+router_v1.register('follow', FollowViewSet, basename='follow')
 
 v1_patterns = [
-    path('', include(router.urls)),
-    path('', include('djoser.urls.jwt')),
-]
-
-router_v2 = routers.DefaultRouter()
-router_v2.register('posts', PostViewSet, basename='posts')
-router_v2.register(
-    r'posts/(?P<post_id>\d+)/comments',
-    CommentViewSet, basename='comments'
-)
-
-v2_patterns = [
-    path('', include(router_v2.urls)),
+    path('', include(router_v1.urls)),
     path('', include('djoser.urls.jwt')),
 ]
 
 urlpatterns = [
     path('v1/', include(v1_patterns)),
-    path('v2/', include(v2_patterns)),
 ]
